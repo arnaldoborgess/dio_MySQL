db4fe184d40e616618f01c7ff256c4d738d2867b
Arnaldo <filhodearnaldo@gmail.com>
2021-04-13 15:48:11 -0300

    first commit

---
+++ dio.sql
@@ -0,0 +1,10 @@
+CREAT TABLE pessoas(
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENTO,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATT
+
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Arnaldo', '1980-09-28')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Pemall', '1986-09-15')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Rafael', '1997-09-22')
